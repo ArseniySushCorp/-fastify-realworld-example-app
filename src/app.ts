0235c4d9bdb41dde0e24a0522a9e58509db7c7ca
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-02-28 00:00:14 +0300

    delete comments

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,10 @@ import AutoLoad, { AutoloadPluginOptions } from "fastify-autoload"
 import { FastifyPluginAsync } from "fastify"
 
 export type AppOptions = {
-  // Place your custom options for app below here.
+  // Custom options for app
 } & Partial<AutoloadPluginOptions>
 
 const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-
   void fastify.register(AutoLoad, {
     dir: join(__dirname, "plugins"),
     options: opts
